4f97476f8034ce6c63e9c3371e38f743a1261e38
Arseniy Chernov <gildmaster23@yandex.ru>
2018-02-20 13:31:11 +0300

    Added functionality to seamlessly pull experiments results from the server to client

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 import xml.etree.ElementTree as ET
 import paramiko as pssh
 import os
+import time
+import shlex
 import main
+import subprocess
 from ssh_wrapper import *
 
 def main(_host, _port, _login, _password):
@@ -14,18 +17,34 @@ def main(_host, _port, _login, _password):
 
         path = "~/DDPG_working/DDPG/"
 
-        #print(ssh_wrap.exec("ls", "-l"))
-        main.exec_scenario("scenario1")
+        print(ssh_wrap.exec("ls", "-l"))
+
+
+        print(ssh_wrap.exec("tar czvf --remove-files DDPG.tar.gz -C ./DDPG_working/DDPG ./experiments")) # Compress (with overwriting the archive)
+
+        print(ssh_wrap.exec("mkdir ./DDPG_working/DDPG/experiments"))
+
+        scp_path = "arseniy@" + str(_host) + ":~/DDPG.tar.gz"
+
+        shell("scp -P 2345 " + scp_path + " ~/Git/DDPG") # Donwload from the client side
+
+        shell("tar xvzf ~/Git/DDPG/DDPG.tar.gz -C ~/Git/DDPG/") # Uncompress
+
+        shell("ls -l ~/Git/DDPG/experiments")
+
 
     finally:
         if ssh:
             ssh.close()
 
-
+    print("scp", "-P 2345", "arseniy@" + str(_host) + ":~/DDPG.tar.gz", "./Git/DDPG")
 
 def exec_scenario(scenario, ssh_wrapper):
     pass
 
+def shell(command):
+    subprocess.run([command], shell=True)
+    time.sleep(2)
 
 if __name__ == '__main__':
     if not os.path.isfile("./credentials.xml"):
