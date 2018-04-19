bf21136a2850e8ec89b63c60862e4c2f81d0f949
Arseniy Chernov <gildmaster23@yandex.ru>
2018-04-19 14:56:32 +0300

    Improve client usage with several servers

--- client.py
+++ client.py
@@ -9,9 +9,8 @@ import argparse
 from automation import EXPERIMENTS_FOLDER
 
 CREDENTIALS_FOLDER = "./credentials/"
-SERVER_FOLDER = "~/DDPG_working/DDPG/"
 
-def main(_host, _port, _login, _password):
+def main(_host, _port, _login, _password, server_folder):
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--download", action="store_true",
                         help="download experiments which you don't have yet")
@@ -27,14 +26,15 @@ def main(_host, _port, _login, _password):
         ssh_wrap = StdPipeWrapper(ssh)
 
         if args.download:
-            a = _get_list_of_folders_serv(ssh_wrap, os.path.join(SERVER_FOLDER, EXPERIMENTS_FOLDER))
+            a = _get_list_of_folders_serv(ssh_wrap, os.path.join(server_folder, EXPERIMENTS_FOLDER))
             b = _get_list_of_folders_client("/media/ars/lin_part2/Git2/DDPG/experiments")
             diff = list(set(a) - set(b))
             if args.remove:
                 print(diff," to be downloaded")
-                download_experiments(ssh_wrap, _host, delete_originals=True, experiments=diff)
+                download_experiments(ssh_wrap, _host, str(_port), _login, server_folder,
+                                     delete_originals=True, experiments=diff)
             else:
-                download_experiments(ssh_wrap, _host, experiments=diff)
+                download_experiments(ssh_wrap, _host, str(_port), _login, server_folder, experiments=diff)
     finally:
         if ssh:
             ssh.close()
@@ -52,9 +52,7 @@ def _get_list_of_folders_client(path):
             result.append(filename)
     return result
 
-def download_experiments(ssh_wrap, _host, experiments=None, delete_originals=False):
-    path = "~/DDPG_working/DDPG/"
-
+def download_experiments(ssh_wrap, _host, _port, _login, server_folder, experiments=None, delete_originals=False):
     print(ssh_wrap.exec("ls", "-l"))
 
     if delete_originals:
@@ -63,7 +61,7 @@ def download_experiments(ssh_wrap, _host, experiments=None, delete_originals=Fal
         do_remove = ""
 
     if experiments is None:
-        print(ssh_wrap.exec("tar cvf " + do_remove + "DDPG.tar -C ./DDPG_working/DDPG ./experiments"))
+        print(ssh_wrap.exec("tar cvf " + do_remove + "DDPG.tar -C " + server_folder + " ./experiments"))
     else:
         if len(experiments) == 0:
             print("Nothing new to download. Exiting...")
@@ -71,16 +69,16 @@ def download_experiments(ssh_wrap, _host, experiments=None, delete_originals=Fal
 
         for i, f in enumerate(experiments):
             if i == 0:
-                print(ssh_wrap.exec("tar cvf " + do_remove + "DDPG.tar -C ./DDPG_working/DDPG ./experiments/" + f))
+                print(ssh_wrap.exec("tar cvf " + do_remove + "DDPG.tar -C " + server_folder + " ./experiments/" + f))
             else:
-                print(ssh_wrap.exec("tar uvf " + do_remove + "DDPG.tar -C ./DDPG_working/DDPG ./experiments/" + f))
+                print(ssh_wrap.exec("tar uvf " + do_remove + "DDPG.tar -C " + server_folder + " ./experiments/" + f))
 
-    print(ssh_wrap.exec("mkdir ./DDPG_working/DDPG/experiments")) #in case if deleted
+    print(ssh_wrap.exec("mkdir " + server_folder + "experiments")) #in case if deleted
 
-    scp_path = "arseniy@" + str(_host) + ":~/DDPG.tar"
+    scp_path = _login + "@" + str(_host) + ":~/DDPG.tar"
 
     #scp -P 2345 arseniy@" + str(_host) + ":~/DDPG.tar /media/ars/lin_part2/Git2/DDPG/
-    shell("scp -P 2345 " + scp_path + " /media/ars/lin_part2/Git2/DDPG/")
+    shell("scp -P " + _port + " " + scp_path + " /media/ars/lin_part2/Git2/DDPG/")
 
     shell("tar xvf /media/ars/lin_part2/Git2/DDPG/DDPG.tar -C /media/ars/lin_part2/Git2/DDPG/")
 
@@ -91,13 +89,14 @@ def shell(command):
     time.sleep(2)
 
 if __name__ == '__main__':
-    if not os.path.isfile(CREDENTIALS_FOLDER + "credentials.xml"):
+    credentials_file = "credentials.xml"
+    if not os.path.isfile(CREDENTIALS_FOLDER + credentials_file):
         print("Create and fill file credentials.xml")
     else:
-        tree = ET.parse(CREDENTIALS_FOLDER + "credentials.xml")
+        tree = ET.parse(CREDENTIALS_FOLDER + credentials_file)
         root = tree.getroot()
         credentials = {elem.tag:elem.text for elem in root}
-        main(credentials['host'], credentials['port'], credentials['login'], credentials['password'])
+        main(credentials['host'], credentials['port'], credentials['login'], credentials['password'], credentials['path'])
 
 
 
