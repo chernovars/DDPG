22afd2ec8d70fabccfa171e9427344d8f1e093da
Arseniy Chernov <gildmaster23@yandex.ru>
2018-04-25 23:20:17 +0300

    Now can download only experiment's recorded statistics, not the gigabytes of training buffers + code generalisation

--- client.py
+++ client.py
@@ -9,11 +9,17 @@ import argparse
 from automation import EXPERIMENTS_FOLDER
 
 CREDENTIALS_FOLDER = "./credentials/"
+CLIENT_PATH = "/media/ars/lin_part2/Git2/DDPG/"
+CLIENT_EXP_PATH = os.path.join(CLIENT_PATH, EXPERIMENTS_FOLDER)
 
 def main(_host, _port, _login, _password, server_folder):
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--download", action="store_true",
                         help="download experiments which you don't have yet")
+    parser.add_argument("-s", "--stats", action="store_true",
+                        help="option for -d to downoald experiments stats only, not networks")
+    parser.add_argument("-e", "--experiment", type=str,
+                        help="specify experiment name")
     parser.add_argument("-R", "--remove", action="store_true",
                         help="shoot video for scenario")
     args = parser.parse_args()
@@ -26,20 +32,24 @@ def main(_host, _port, _login, _password, server_folder):
         ssh_wrap = StdPipeWrapper(ssh)
 
         if args.download:
-            a = _get_list_of_folders_serv(ssh_wrap, os.path.join(server_folder, EXPERIMENTS_FOLDER))
-            b = _get_list_of_folders_client("/media/ars/lin_part2/Git2/DDPG/experiments")
-            diff = list(set(a) - set(b))
-            if args.remove:
-                print(diff," to be downloaded")
-                download_experiments(ssh_wrap, _host, str(_port), _login, server_folder,
-                                     delete_originals=True, experiments=diff)
+            list_of_folders_serv = _get_list_of_folders_serv(ssh_wrap, os.path.join(server_folder, EXPERIMENTS_FOLDER))
+            if args.stats:
+                exp_full_path = os.path.join(server_folder, EXPERIMENTS_FOLDER, args.experiment)
+                stats_names = ssh_wrap.exec("ls -p " + exp_full_path + " | grep -v /").split("\n")
+                download_stats(_host, str(_port), _login, server_folder, args.experiment, stats_names)
             else:
-                download_experiments(ssh_wrap, _host, str(_port), _login, server_folder, experiments=diff)
+                folders_client = _get_list_of_folders_client(CLIENT_EXP_PATH)
+                diff = list(set(list_of_folders_serv) - set(folders_client))
+                if args.remove:
+                    print(diff," to be downloaded")
+                    download_experiments(ssh_wrap, _host, str(_port), _login, server_folder,
+                                         delete_originals=True, experiments=diff)
+                else:
+                    download_experiments(ssh_wrap, _host, str(_port), _login, server_folder, experiments=diff)
     finally:
         if ssh:
             ssh.close()
 
-#    print("scp", "-P 2345", "arseniy@" + str(_host) + ":~/DDPG.tar.gz", "./Git/DDPG")
 
 def _get_list_of_folders_serv(ssh_wrap, path):
     return ssh_wrap.exec("ls " + path).split("\n")[:-1]
@@ -74,19 +84,24 @@ def download_experiments(ssh_wrap, _host, _port, _login, server_folder, experime
                 print(ssh_wrap.exec("tar uvf " + do_remove + "DDPG.tar -C " + server_folder + " ./experiments/" + f))
 
     print(ssh_wrap.exec("mkdir " + server_folder + "experiments")) #in case if deleted
-
     scp_path = _login + "@" + str(_host) + ":~/DDPG.tar"
+    shell("scp -P " + _port + " " + scp_path + " " + CLIENT_PATH)
+    shell("tar xvf "+ CLIENT_PATH +" DDPG.tar -C " + CLIENT_PATH)
+    shell("ls -l " + CLIENT_EXP_PATH)
 
-    #scp -P 2345 arseniy@" + str(_host) + ":~/DDPG.tar /media/ars/lin_part2/Git2/DDPG/
-    shell("scp -P " + _port + " " + scp_path + " /media/ars/lin_part2/Git2/DDPG/")
 
-    shell("tar xvf /media/ars/lin_part2/Git2/DDPG/DDPG.tar -C /media/ars/lin_part2/Git2/DDPG/")
+def download_stats(_host, _port, _login, server_folder, experiment_name, stat_names):
+    shell("mkdir -p " + os.path.join(CLIENT_EXP_PATH, experiment_name))
+    print("Files to download: ", len(stat_names))
+    for i, s in enumerate(stat_names):
+        print(i)
+        scp_path = _login + "@" + str(_host) + ":" + server_folder + "experiments/" + experiment_name + "/" + s
+        shell("scp -P " + _port + " " + scp_path + " /media/ars/lin_part2/Git2/DDPG/experiments/" + \
+              experiment_name + '/' + s, sleep=0.1)
 
-    shell("ls -l /media/ars/lin_part2/Git2/DDPG/experiments")
-
-def shell(command):
+def shell(command, sleep=2.0):
     subprocess.run([command], shell=True)
-    time.sleep(2)
+    time.sleep(sleep)
 
 if __name__ == '__main__':
     credentials_file = "credentials.xml"
